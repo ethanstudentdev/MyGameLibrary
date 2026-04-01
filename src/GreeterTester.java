789cc74dd7506007ecc7bc18672ad8570fe48ad8
Nathaniel-Cha <nkc0005@uah.edu>
2026-03-31 21:56:10 -0500

    Added basic main window and login popup window

--- src/GreeterTester.java
+++
@@ -1,8 +0,0 @@
-public class GreeterTester
-{
-    public void main(){
-        Greeter worldGreeter = new Greeter("World");
-        String greeting = worldGreeter.sayHello();
-        System.out.println(greeting);
-    }
-}
